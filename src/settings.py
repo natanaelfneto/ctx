740b9bf0a28cdb72144f31a5b6e2563906cb4f56
Natanael F Neto <natanaelfneto@outlook.com>
2019-02-21 19:15:00 -0300

    clean up changes for first deploy

--- src/settings.py
+++ src/settings.py
@@ -65,10 +65,21 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'src.wsgi.application'
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ctxadmin',
+        'USER': 'postgres',
+        'PASSWORD': 'pgadmin',
+        'HOST': '127.0.0.1',
+        'PORT': '5433',
     }
 }
 
